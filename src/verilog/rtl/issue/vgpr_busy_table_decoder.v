fc36ba107ed1a77423e2612cd5749c6e2eb491cb
Ziliang Guo <zguo2@wisc.edu>
2016-02-16 00:11:38 -0600

    Duplicate sgpr busy table decoder fix in the vgpr's.

--- src/verilog/rtl/issue/vgpr_busy_table_decoder.v
+++ src/verilog/rtl/issue/vgpr_busy_table_decoder.v
@@ -13,6 +13,6 @@ module vgpr_busy_table_decoder
    output [`NUMBER_VGPR-1:0] 	output_decoded;
    wire [`NUMBER_VGPR+4-1:0] 	output_decoded_2;
    
-   assign output_decoded_2 = vgpr_valid << vgpr_addr;
+   assign output_decoded_2 = (|vgpr_valid) ? vgpr_valid << vgpr_addr : 1028'd0;
    assign output_decoded = output_decoded_2[`NUMBER_VGPR-1:0] | { {(`NUMBER_VGPR-4){1'b0}},output_decoded_2[`NUMBER_VGPR+4-1:`NUMBER_VGPR]};
 endmodule
