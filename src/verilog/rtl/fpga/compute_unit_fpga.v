fe7c3850364e0c694ceac5e60c03c4432d745ad5
d1duarte <d1duarte@users.noreply.github.com>
2016-03-08 14:45:09 +0000

    Added vector register access from microblaze

--- src/verilog/rtl/fpga/compute_unit_fpga.v
+++ src/verilog/rtl/fpga/compute_unit_fpga.v
@@ -35,6 +35,10 @@ module compute_unit_fpga #
   output wire [2047:0] singleVectorData_in,
   input wire [9:0] singleVectorBaseAddress_out,
   
+  input wire [2047:0] singleVectorWrData_out,
+  input wire [63:0] singleVectorWrDataMask_out,
+  input wire [3:0] singleVectorWrEn_out,
+  
   input wire execute_out,
   input wire executeStart_out,
   
@@ -81,7 +85,10 @@ wire [31:0] instruction_buff_out_b;
 // SGPR registers
 reg [31:0] gprCommand;
 wire [15:0] sgpr_select_fu;
+wire [15:0] vgpr_select_fu;
+
 wire [3:0] lsu2sgpr_dest_wr_en_thru;
+wire [3:0] lsu2vgpr_dest_wr_en_thru;
 
 wire  buff2fetchwave_ack, cu2dispatch_wf_done, decode2issue_barrier,
       decode2issue_branch, decode2issue_exec_rd, decode2issue_exec_wr, decode2issue_m0_rd,
@@ -231,6 +238,9 @@ wire [9:0] lsu2sgpr_source1_addr_thru;
 wire [9:0] lsu2sgpr_dest_addr_thru;
 wire [127:0] lsu2sgpr_dest_data_thru;
 wire lsu2sgpr_source1_rd_en_thru;
+wire [9:0] lsu2vgpr_dest_addr_thru;
+wire [2047:0] lsu2vgpr_dest_data_thru;
+wire [63:0] lsu2vgpr_dest_wr_mask_thru;
 
 wire lsu2vgpr_source1_rd_en_thru;
 wire [9:0] lsu2vgpr_source1_addr_thru;
@@ -289,8 +299,14 @@ assign lsu2sgpr_source1_rd_en_thru = execute_out ? lsu2sgpr_source1_rd_en : ~(|l
 assign lsu2sgpr_dest_wr_en_thru = execute_out ? lsu2sgpr_dest_wr_en : lsu2sgpr_dest_wr_en_out;
 assign lsu2sgpr_dest_data_thru = execute_out ? lsu2sgpr_dest_data : {quadData3_out, quadData2_out, quadData1_out, quadData0_out};
 
-assign lsu2vgpr_source1_rd_en_thru = execute_out ? lsu2vgpr_source1_rd_en : 1'b1;
+assign lsu2vgpr_source1_rd_en_thru = execute_out ? lsu2vgpr_source1_rd_en : ~(|singleVectorWrEn_out);
 assign lsu2vgpr_source1_addr_thru  = execute_out ? lsu2vgpr_source1_addr : singleVectorBaseAddress_out;
+
+assign lsu2vgpr_dest_addr_thru = execute_out ? lsu2vgpr_source1_addr : singleVectorBaseAddress_out;
+assign vgpr_select_fu = execute_out ? rfa2execvgprsgpr_select_fu : {7'd0,  1'b1, 8'd0};
+assign lsu2vgpr_dest_wr_en_thru = execute_out ? lsu2vgpr_dest_wr_en : singleVectorWrEn_out;
+assign lsu2vgpr_dest_data_thru = execute_out ? lsu2vgpr_dest_data : singleVectorWrData_out;
+assign lsu2vgpr_dest_wr_mask_thru = execute_out ? lsu2vgpr_dest_wr_mask : singleVectorWrDataMask_out;
 
 fetch fetch0 (
   // Unit that fetches instructions from a wavefront chosen by the wavepool
@@ -424,10 +440,10 @@ fetch fetch0 (
   .simf3_wr_mask(simf3_2vgpr_wr_mask),
   .lsu_source1_addr(lsu2vgpr_source1_addr_thru),
   .lsu_source2_addr(lsu2vgpr_source2_addr),
-  .lsu_dest_addr(lsu2vgpr_dest_addr),
-  .lsu_dest_data(lsu2vgpr_dest_data),
-  .lsu_dest_wr_en(lsu2vgpr_dest_wr_en),
-  .lsu_dest_wr_mask(lsu2vgpr_dest_wr_mask),
+  .lsu_dest_addr(lsu2vgpr_dest_addr_thru),
+  .lsu_dest_data(lsu2vgpr_dest_data_thru),
+  .lsu_dest_wr_en(lsu2vgpr_dest_wr_en_thru),
+  .lsu_dest_wr_mask(lsu2vgpr_dest_wr_mask_thru),
   .lsu_instr_done_wfid(lsu2vgpr_instr_done_wfid),
   .lsu_instr_done(lsu2vgpr_instr_done),
   .lsu_source1_rd_en(lsu2vgpr_source1_rd_en_thru),
@@ -448,7 +464,7 @@ fetch fetch0 (
   .simf1_instr_done(simf1_2vgpr_instr_done),
   .simf2_instr_done(simf2_2vgpr_instr_done),
   .simf3_instr_done(simf3_2vgpr_instr_done),
-  .rfa_select_fu(rfa2execvgprsgpr_select_fu),
+  .rfa_select_fu(vgpr_select_fu),
   //  Outputs
   .simd_source1_data(vgpr2simd_source1_data),
   .simd_source2_data(vgpr2simd_source2_data),
